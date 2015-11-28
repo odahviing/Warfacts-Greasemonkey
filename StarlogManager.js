7f43570fa7e78dea889d04a551c8e0500900cadc
IdanCohen <idanc@bugsec.com>
2015-11-28 20:22:15 +0200

    no message

--- StarlogManager.js
+++ StarlogManager.js
@@ -2,17 +2,34 @@
 // @name        Starlog Manager
 // @namespace   bitbucket.org/Odahviing
 // @include     http://www.war-facts.com/starlog.php*
-// @version     1.1
-// @grant       none
+// @include     http://www.war-facts.com/player.php
+// @version     1.11
+// @grant       GM_getValue
+// @grant       GM_setValue
 // @description Starlog addon
 // ==/UserScript==
 
-// Version 1.0 - Inital Script - Player filter + Real Time
-// Version 1.1 - Minor Changes on menus
+// Version 1.0  - Inital Script - Player filter + Real Time
+// Version 1.1  - Minor Changes on menus
+// Version 1.11 - Bug fix - not showing "hour"
 
 // Features So Far
 // --- Ability to filter with "player" messages only
 
+
+function saveGroups()
+{
+  var groupsNames = document.getElementsByName('editstarloggroupname');
+  var groupsId = document.getElementsByName('editstarloggroup');
+  
+  var arrayList = [];
+  for (var index = 0 ; index < groupsNames.length; index ++)
+  {
+    var newItem = [groupsId[index].value, groupsNames[index].value];
+    arrayList.push(newItem);
+  }
+  GM_setValue("stargroupNames", arrayList);
+}
 
 function loadPlayerMessages()
 {
@@ -41,6 +58,12 @@ function loadPlayerMessages()
 // Based of guardian21 script
 function addRealDate()
 {
+    //Set Time zone Difference
+    var currentdate = new Date(); 
+    var timeZoneDiff = currentdate.getTimezoneOffset() / 60;	// Difference between user time and UTC time in hours
+      timeZoneDiff = -timeZoneDiff;	// I want to know user compared to utc, not utc compared to user
+      timeZoneDiff += 5;	//Difference between UTC and EST (server is in EST)  
+  
    var divs = document.getElementById("midcolumn").getElementsByTagName("div")[0].getElementsByTagName("div");
    var i, current_div, realTimeEST,timetext,timeDateSpan,temp;
 
@@ -95,7 +118,7 @@ function addRealDate()
 
 function loadOnlyPlayer()
 {
-  window.location.replace("http://www.war-facts.com/starlog.php?#1");
+  window.location.replace("http://www.war-facts.com/starlog.php?#0");
 }
 
 function onlyPlayer()
@@ -129,6 +152,78 @@ function onlyPlayer()
   for (index = 0; index < removeList.length; index++) removeList[index].remove();
 }
 
+function changeStatus(name, status)
+{
+  if (status == true) return;
+    
+  
+  var allMessages = document.getElementsByClassName('fullwidth dark tbborder');
+  var removeList = [];
+  
+  for (var index = 0; index < allMessages.length; index ++)
+  {
+    var innerText = allMessages[index].getElementsByClassName('groupmessageheader');
+ alert(innerText[0].innerHTML);
+    alert(name);
+    var index = innerText[0].innerHTML.indexOf("syncGroup" + name);
+    
+    
+    alert("index is: " + index );
+    if (index != -1) removeList.push(allMessages[index]);
+  }
+  
+  for (index = 0; index < removeList.length; index++) removeList[index].remove();
+}
+
+function loadStarGroup(groupNumber)
+{
+  window.location.replace("http://www.war-facts.com/starlog.php?#" + groupNumber);
+}
+function addCheckBoxs()
+{ 
+  var allMessages = document.getElementsByClassName('left starlog_right box dark padding5 minheight60');
+  var  arrayList = GM_getValue("stargroupNames");
+  if (arrayList == undefined) return;
+  var addText = "<div class='left starlog_left box light padding10 hide_mobile'></div><div class='left starlog_right box dark padding5 minheight60'>";
+  
+  for (var index = 0; index < arrayList.length; index++)
+  {
+    var newText = "<a class='darkbutton smalltext' id='text" + arrayList[index][0] + "'>" + arrayList[index][1] + "</a>";
+    addText += newText;
+  }
+  addText += "</div>";
+  allMessages[0].outerHTML = allMessages[0].outerHTML + addText;
+  
+  for (index = 0; index < arrayList.length; index++)
+    {
+      var label = document.getElementById('text' + arrayList[index][0]);
+      label.addEventListener("click", function(){alert('1');loadStarGroup(arrayList[index][0]);alert('1');}, false);
+    }
+}
+
+
+
+function keepOnlyGroup(groupKey)
+{
+  var allMessages = document.getElementsByClassName('fullwidth dark tbborder');
+  var removeList = [];
+  
+  for (var index = 0; index < allMessages.length; index ++)
+  {
+    var isGroup = allMessages[index].getElementsByClassName('groupmessageheader');
+    if (isGroup.length != 0) 
+    {
+         var value = isGroup[0].getElementsByTagName("a")[0].href;
+         var group = value.substring(value.indexOf('(') + 1, ')' - 1);
+         if (group != groupKey) removeList.push(allMessages[index]);
+
+    }
+  }
+  
+  for (index = 0; index < removeList.length; index++) removeList[index].remove(); 
+}
+
+
 function removePlayerMessages()
 {
   var allMessages = document.getElementsByClassName('fullwidth dark tbborder');
@@ -144,20 +239,33 @@ function removePlayerMessages()
 }
 
 // Loading
-//Set Time zone Difference
-var currentdate = new Date(); 
-var timeZoneDiff = currentdate.getTimezoneOffset() / 60;	// Difference between user time and UTC time in hours
-	timeZoneDiff = -timeZoneDiff;	// I want to know user compared to utc, not utc compared to user
-	timeZoneDiff += 5;	//Difference between UTC and EST (server is in EST)
-var groups = ["fleet", "explore", "colony", "empire", "science"];
-
-// Start
-loadPlayerMessages();
-var index = document.URL.indexOf("?");
-if (index != -1)
+if (document.URL.indexOf("player.php") != -1)
+  saveGroups();
+else
 {
 
-  var getType = document.URL.substring(index + 1);    
-  if (getType == "type=2") removePlayerMessages();
-  else if (getType =="#1") onlyPlayer();
+  // Start
+  loadPlayerMessages();
+  //addCheckBoxs();
+  var index = document.URL.indexOf("?");
+  if (index != -1)
+  {
+
+    var groups = ["fleet", "explore", "colony", "empire", "science"];
+
+    var getType = document.URL.substring(index + 1);    
+    if (getType == "type=2") 
+    {
+        removePlayerMessages();
+        
+    }
+    else if (getType =="#0") 
+      onlyPlayer();
+    else if (getType.indexOf("#") != -1)
+    {
+    //  removePlayerMessages();
+     //   keepOnlyGroup(getType.substring(getType.indexOf("#") +1));
+    }
+
+  }
 }
