97472988801376872405ea34adcd0fb73e5860cb
IdanCohen <idanc@bugsec.com>
2015-11-29 23:42:50 +0200

    no message

--- Coordinator.js
+++ Coordinator.js
@@ -4,7 +4,7 @@
 // @description Small fleet utility to increase effective
 // @include     http://www.war-facts.com/fleet.php?fleet=*
 // @include     http://www.war-facts.com/fleet/*
-// @version     1.22
+// @version     1.3
 // @grant       none
 // ==/UserScript==
 
@@ -12,23 +12,33 @@
 // Version 1.0  - Basic version - will make it much bigger
 // Version 1.1  - Added random cords generator
 // Version 1.2  - Limiting Z axi + Fix issue with "next explorer script"
-// Version 1.2x - Bug fix for some of the fleet status
+// Version 1.21 - Bug fix for some of the fleet status
+// Version 1.3  - Bug fix to all kind of fleets status
 
 
 window.setTimeout(main,500); // For loading issues
 
+function detectLine(lines)
+{
+   var startLine = 4;
+   var ahref = lines[4].getElementsByTagName('a')[0];
+  
+  if (ahref != undefined  && ahref.innerHTML.indexOf('global') != -1) return ahref;
+   var ahref = lines[5].getElementsByTagName('a')[0];
+    if (ahref != undefined && ahref.innerHTML.indexOf('global') != -1) return ahref;
+   var ahref = lines[6].getElementsByTagName('a')[0];
+   if (ahref != undefined && ahref.innerHTML.indexOf('global') != -1) return ahref;
+}
 
 function coordCopy()
 {
   // Get cords and copy
   var lines = document.getElementsByClassName('light tbborder padding5');
+  var ahref = detectLine(lines);
   var change = document.getElementById('textcoords').value;
   
-  var ahref = lines[4].getElementsByTagName('a')[0];
-  if (ahref.innerHTML.indexOf("local") != -1)
-     ahref = lines[5].getElementsByTagName('a')[0];
-  
   var coords = ahref.innerHTML.substring(0, ahref.innerHTML.indexOf('g')-1);  
+  
   if (change == "")
   {
     document.getElementById('xyz').value=coords;  
