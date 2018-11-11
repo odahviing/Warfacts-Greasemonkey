fd8e808f852828b3a65ae6282135e93f3454ec3a
Idan Cohen <idanc20@gmail.com>
2018-11-11 02:00:23 +0200

    Version 2.3 - Auto Exploration

--- auto-explorer/auto-exploration.user.js
+++ auto-explorer/auto-exploration.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         Auto Exploration
-// @namespace    bitbucket.org/Odahviing
-// @version      2.2
-// @description  Ease your exploration mission with smart logic
+// @namespace    github.com/odahviing/warfacts-greasemonkey
+// @version      2.3
+// @description  Ease your exploration mission with automatic smart logic for probing
 // @author       Odahviing
 // @match        http://www.war-facts.com/fleet*
 // @exclude      http://www.war-facts.com/fleet.php?mtype*
@@ -23,28 +23,153 @@
 // Version 2.2
 // -- Fix a stupid bug that ignore places that had x value of aboue 300000 (so almost every galaxy)
 
+// Version 2.3 - Code Beautifier
+
 // TODO:
 // -- Fix Numbering ! My cords is off
 // -- Find a Way to NOT go to the same planet (and then priotize by distance)
 
+// Settings
 
 var JavaScriptV2 = true; // It seems to work on every UI and faster, so no need to use the old code.
-
-var minimumFuelAmount = 50000000;
-var maxCount = 5;
-var safeDistance = 2000000;
-var zoomOutNumber = 3000;
+var minimumFuelAmount = 50000000; // Will go fuel if below
+var safeDistance = 2000000; // If the system is 2m km from the fleet range, we won't go there (need to fix the math a-bit)
+var zoomOutNumber = 3000; // The zoomout after failing finding unexplored system.
 
 (function() {
     'use strict';
     setTimeout(loadButton, 600);
     setTimeout(pressTheButton, 100);
-    // Your code here...
 })();
+
+// On-Load Functions
+
+function loadButton() {
+    let newButton = document.createElement('input');
+    newButton.type = 'button'
+    newButton.value = 'Explore'
+    newButton.style = 'width: 130px;'
+    newButton.className = 'darkbutton dangerbutton';
+    newButton.addEventListener("click", run);
+    var buttonPlace = document.getElementsByClassName('iBlock tbborder padding5 fullwidth light')[0];
+    if (buttonPlace)
+        buttonPlace.insertBefore(newButton, null);
+}
 
 function pressTheButton() {
     if (document.URL.indexOf('callback=1') > 0) getMission('launch')
 }
+
+// End On-Load Functions
+
+// Fleet Functions
+
+var fleetNumber;
+var fleetRange;
+var fleetFuel;
+
+function getFleetNumber() {
+    let tmpUrl = document.URL;
+    return tmpUrl.indexOf('?') > 0 ?
+        tmpUrl.split('?')[1].split('=')[1] :
+        tmpUrl.split('/')[4];
+}
+
+function getFleetRange() {
+    let elem = document.getElementById('fleetRange').innerHTML;
+    return parseInt(elem.replace(',','').replace(',','').replace(',','')) - safeDistance;
+}
+
+function getFuelAmount() {
+    var spanItem = document.getElementById('fleetRange').innerHTML.replace(',','').replace(',','').replace(',','');
+    return parseInt(spanItem);
+}
+
+function run() {
+    fleetNumber = getFleetNumber();
+    fleetRange = getFleetRange();
+    fleetFuel = getFuelAmount();
+    findWH().then(function(status) {
+        var baseUrlLink = getMapUrl();
+        findUnexplored(baseUrlLink).then(function(result) {
+            if (result == false) {
+                if (fleetFuel < minimumFuelAmount) {
+                    let optionsElements = document.getElementById('target1').getElementsByTagName('option');
+                    if (optionsElements.length == 2)
+                        return alert('No Place To Re-fuel');
+                    else {
+                        let finalPlanet = optionsElements[2].value.split(',')[1]
+                        document.getElementById('target1').value='tworld,' + finalPlanet;
+                        getMission("verify", "target1")
+                        return setTimeout(getMission('launch'),100);
+                    }
+                }
+                else {
+                    return alert('No System Were Found');
+                }
+            }
+            else { // Found an Unexplored system
+                window.location = result;
+            }
+        }).catch(function (reason) {
+            alert(reason);
+        return false;
+        })
+    }).catch(function (reason) {
+        console.log('Stopping: ' + reason);
+        return false;
+    });
+}
+
+function findWH(){
+    var at100 = checkIf100x3();
+    return new Promise(function (fulfill, reject){
+        if (at100 == true)
+        {
+            checkForWH().then(function(isWH) {
+                if (isWH == true){
+                    alert('Found Wormhole!');
+                    return reject('Wormhole');
+                }
+
+                let fuelAmount = getFuelAmount();
+
+
+                return fulfill(true);
+            });
+        }
+        else
+            return fulfill(true);
+    });
+}
+
+function checkIf100x3() {
+    var firstLine = document.getElementsByClassName('padding5 light tbborder')[0];
+    if(firstLine.getElementsByTagName('span')[0].innerHTML == 'System: ')
+        return true;
+    else
+        return false;
+}
+
+function checkForWH(){
+    return new Promise(function (fulfill, reject){
+        var link = document.getElementsByClassName('darkbutton')[2].onclick.toString();
+        link = "http://www.war-facts.com/extras/scan.php" + link.substring(link.indexOf('?'), link.indexOf("')"));
+        divAjaxRequest("GET", link, true, true, null).then(function(newDiv) {
+            var All = newDiv.getElementsByTagName('i');
+            for (let i = 0 ; i < All.length; i++)
+            {
+                if (All[i].innerHTML == 'Wormhole!')
+                    return fulfill(true);
+            }
+            return fulfill(false);
+        });
+    });
+}
+
+// End Fleet Page Functions
+
+// Find Unexplored System Functions
 
 function findUnexplored(url) {
     return new Promise(function (fulfill, reject){
@@ -59,7 +184,25 @@ function findUnexplored(url) {
     });
 }
 
+function getMapUrl() {
+    var allNavs = document.getElementById('navData').getElementsByClassName('light tbborder padding5');
+    var textItem = allNavs[allNavs.length - 1].innerHTML;
+    let url = 'http://www.war-facts.com/extras/view_universe.php' + textItem.substring(textItem.indexOf('?'), textItem.indexOf("')")).replace('&amp;','&').replace('&amp;','&').replace('&amp;','&');
+    return url;
+}
+
+function replaceDeepUrl(url, zoomOut) {
+    let leftSide = url.substring(0,url.indexOf("&z=")+3);
+    url = url.replace(leftSide, "");
+    let finalParts = url.split('&');
+    let newNumberString = (parseInt(finalParts[0]) + zoomOut);
+    url = leftSide + newNumberString + "&" + finalParts[1];
+    return url;
+}
+
 // pseudo 3D CSS - Functions
+
+var maxCount = 5;
 
 function findUnexploredPseudo(url, count = 0) {
     return new Promise(function (fulfill, reject){
@@ -113,9 +256,6 @@ function extractFinalLink(url) {
 // Javascript V2 - Functions
 
 var mainCord;
-var fleetNumber;
-var fleetRange;
-var fleetFuel;
 
 function buildCordsObject(x,y,z) {
     return {X : parseInt(x), Y: parseInt(y), Z: parseInt(z)}
@@ -174,136 +314,7 @@ function extractLink(url) {
     return `http://www.war-facts.com/ajax/jsmap_postload_v2.php?centerX=${x}&centerY=${y}&centerZ=${z}&displayRange=25000`;
 }
 
-// Fleet Page Functions
-
-function loadButton() {
-    let newButton = document.createElement('input');
-    newButton.type = 'button'
-    newButton.value = 'Explore'
-    newButton.style = 'width: 130px;'
-    newButton.className = 'darkbutton dangerbutton';
-    newButton.addEventListener("click", run);
-    var buttonPlace = document.getElementsByClassName('iBlock tbborder padding5 fullwidth light')[0];
-    if (buttonPlace)
-        buttonPlace.insertBefore(newButton, null);
-}
-
-function getFleetNumber() {
-    let tmpUrl = document.URL;
-    return tmpUrl.indexOf('?') > 0 ?
-        tmpUrl.split('?')[1].split('=')[1] :
-        tmpUrl.split('/')[4];
-}
-function getFleetRange() {
-    let elem = document.getElementById('fleetRange').innerHTML;
-    return parseInt(elem.replace(',','').replace(',','').replace(',','')) - safeDistance;
-}
-
-function run() {
-    fleetNumber = getFleetNumber();
-    fleetRange = getFleetRange();
-    fleetFuel = getFuelAmount();
-    findWH().then(function(status) {
-        var baseUrlLink = getMapUrl();
-        findUnexplored(baseUrlLink).then(function(result) {
-            if (result == false) {
-                if (fleetFuel < minimumFuelAmount) {
-                    let optionsElements = document.getElementById('target1').getElementsByTagName('option');
-                    if (optionsElements.length == 2)
-                        return alert('No Place To Re-fuel');
-                    else {
-                        let finalPlanet = optionsElements[2].value.split(',')[1]
-                        document.getElementById('target1').value='tworld,' + finalPlanet;
-                        getMission("verify", "target1")
-                        return setTimeout(getMission('launch'),100);
-                    }
-                }
-                else {
-                    return alert('No System Were Found');
-                }
-            }
-            else { // Found an Unexplored system
-                window.location = result;
-            }
-        }).catch(function (reason) {
-            alert(reason);
-        return false;
-        })
-    }).catch(function (reason) {
-        console.log('Stopping: ' + reason);
-        return false;
-    });
-}
-
-function getMapUrl() {
-    var allNavs = document.getElementById('navData').getElementsByClassName('light tbborder padding5');
-    var textItem = allNavs[allNavs.length - 1].innerHTML;
-    let url = 'http://www.war-facts.com/extras/view_universe.php' + textItem.substring(textItem.indexOf('?'), textItem.indexOf("')")).replace('&amp;','&').replace('&amp;','&').replace('&amp;','&');
-    return url;
-}
-
-function checkForWH(){
-    return new Promise(function (fulfill, reject){
-        var link = document.getElementsByClassName('darkbutton')[2].onclick.toString();
-        link = "http://www.war-facts.com/extras/scan.php" + link.substring(link.indexOf('?'), link.indexOf("')"));
-        divAjaxRequest("GET", link, true, true, null).then(function(newDiv) {
-            var All = newDiv.getElementsByTagName('i');
-            for (let i = 0 ; i < All.length; i++)
-            {
-                if (All[i].innerHTML == 'Wormhole!')
-                    return fulfill(true);
-            }
-            return fulfill(false);
-        });
-    });
-}
-
-function checkIf100x3() {
-    var firstLine = document.getElementsByClassName('padding5 light tbborder')[0];
-    if(firstLine.getElementsByTagName('span')[0].innerHTML == 'System: ')
-        return true;
-    else
-        return false;
-}
-
-function findWH(){
-    var at100 = checkIf100x3();
-    return new Promise(function (fulfill, reject){
-        if (at100 == true)
-        {
-            checkForWH().then(function(isWH) {
-                if (isWH == true){
-                    alert('Found Wormhole!');
-                    return reject('Wormhole');
-                }
-
-                let fuelAmount = getFuelAmount();
-
-
-                return fulfill(true);
-            });
-        }
-        else
-            return fulfill(true);
-    });
-}
-
-function getFuelAmount() {
-    var spanItem = document.getElementById('fleetRange').innerHTML.replace(',','').replace(',','').replace(',','');
-    return parseInt(spanItem);
-}
-
-// End Fleet Page Functions
-
-function replaceDeepUrl(url, zoomOut) {
-    let leftSide = url.substring(0,url.indexOf("&z=")+3);
-    url = url.replace(leftSide, "");
-    let finalParts = url.split('&');
-    let newNumberString = (parseInt(finalParts[0]) + zoomOut);
-    url = leftSide + newNumberString + "&" + finalParts[1];
-    return url;
-
-}
+// End Find Unexplored System Functions
 
 // Utilities Functions
 
@@ -347,5 +358,7 @@ function createShuffleArray(num) {
     }
     return array;
 }
+
+// End Utilities Functions
 
 // End Utilities Functions
