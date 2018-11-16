b2a65083841be8e5614b0aa063f29bbfed235834
Idan Cohen <idanc20@gmail.com>
2018-11-17 01:21:38 +0200

    Version 0.1-Transfer to planet

---
+++ transfer-to-planet/transfer-to-planet.user.js
@@ -0,0 +1,59 @@
+// ==UserScript==
+// @name         Transfer to Planet
+// @namespace    github.com/odahviing
+// @version      0.1
+// @description  Provide option to send fleet to planet directly
+// @author       Odahviing
+// @match        http://www.war-facts.com/fleet.php?*
+// @grant        none
+// ==/UserScript==
+
+// Version 0.1 - Beta Version
+
+(function() {
+    'use strict';
+    if (isCallBack() == true)
+        setTimeout(pressTheButton, 200);
+    else
+        prepareScript()
+})();
+
+function prepareScript() {
+    addOption();
+    hookButton();
+}
+
+function addOption() {
+    var fleetOption = document.getElementById('tpos');
+    fleetOption.innerHTML = fleetOption.innerHTML + `<option value="planet">Planet</option>`
+
+}
+
+function hookButton() {
+    originalGetMission = getMission;
+    getMission = function(str1, str2) {
+        if (document.getElementById('tpos').value == 'planet')
+        {
+            let planetId = document.getElementById('xyz').value;
+            let fleetId = getFleetNumber();
+            window.location = `/fleet.php?tworld=${planetId}&fleet=${fleetId}#press=1`;
+        }
+        else
+            originalGetMission('verify', 'c');
+    }
+}
+
+function getFleetNumber() {
+    let tmpUrl = document.URL;
+    return tmpUrl.indexOf('?') > 0 ?
+        tmpUrl.split('?')[1].split('=')[1] :
+        tmpUrl.split('/')[4];
+}
+
+function pressTheButton() {
+    getMission('launch');
+}
+
+function isCallBack() {
+    return document.URL.indexOf('press=1') > 0;
+}
