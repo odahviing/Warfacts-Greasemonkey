a7a61f425440b449a00919656a2d71caeb13c942
Idan Cohen <idanc20@gmail.com>
2018-12-03 09:57:10 +0200

    # 4 Directions -> Diff Range Some internal changes

--- sensor-net-manager/Sensor Net Manager.user.js
+++ sensor-net-manager/Sensor Net Manager.user.js
@@ -1,13 +1,17 @@
 // ==UserScript==
 // @name         Sensor Net Manager
 // @namespace    github.com/odahviing/warfacts
-// @version      0.3
+// @version      0.5
 // @description  Next Generation Sensor Net
 // @author       Odahviing
 // @match        http://www.war-facts.com/sensorArray.php
 // @grant        GM_getValue
 // @grant        GM_setValue
 // ==/UserScript==
+
+// TODO:
+// -- Add Bookmark all option
+// -- Add Raw muiltiple fleets name + send
 
 (function() {
     'use strict';
@@ -48,7 +52,7 @@ function readInputs(_this) {
     var networkName = allInputs[0].value;
     var scannerLevel = allInputs[1].value;
     var coreCords = Cords(allInputs[2].value, allInputs[3].value, allInputs[4].value);
-    var range = allInputs[5].value;
+    var range = Range(allInputs[5].value,allInputs[6].value,allInputs[7].value,allInputs[8].value);
 
     var networkData = Network(networkName, scannerLevel, coreCords, range);
     return networkData;
@@ -73,8 +77,7 @@ function view(){
 
     let newData = "";
     for (let i = 0; i < allPoints.length; i++) {
-        var distance = getDistance(networkData.Cords, allPoints[i]);
-        newData = newData + newRaw(i+1, allPoints[i], distance);
+        newData = newData + newRaw(i+1, allPoints[i]);
     }
 
     document.getElementById('data-holder').innerHTML = addHeader() + newData;
@@ -164,55 +167,92 @@ function getAllCords(cords, level, fuel) {
     let scanPoints = sTc(level);
     let scanRange = cTr(scanPoints);
 
-    let count = Math.ceil(fuel / (scanRange * 2), 1);
+    let westCount = Math.ceil(fuel.West / (scanRange * 2), 1);
+    let northCount = Math.ceil(fuel.North / (scanRange * 2), 1);
+    let eastCount = Math.ceil(fuel.East / (scanRange * 2), 1);
+    let southCount = Math.ceil(fuel.South / (scanRange * 2), 1);
 
     var allCords = [];
 
-    allCords = allCords.concat(buildRow(cords, scanPoints));
-    for (let i = 1; i < count; i++) {
-        allCords = allCords.concat(buildRow(addX(clone(cords), 2 * i * scanPoints), scanPoints));
-        allCords = allCords.concat(buildRow(addX(clone(cords), 2 * i * -scanPoints), scanPoints));
-        allCords = allCords.concat(buildRow(addY(clone(cords), 2 * i * scanPoints), scanPoints));
-        allCords = allCords.concat(buildRow(addY(clone(cords), 2 * i * -scanPoints), scanPoints));
-    }
+    allCords = allCords.concat(mainCords(cords, scanPoints));
+    allCords = allCords.concat(generateRows(clone(cords), -2 * scanPoints, 0, westCount, fuel.West, scanPoints));
+    allCords = allCords.concat(generateRows(clone(cords), 0, 2 * scanPoints, northCount, fuel.North, scanPoints));
+    allCords = allCords.concat(generateRows(clone(cords), 2 * scanPoints, 0, eastCount, fuel.East, scanPoints));
+    allCords = allCords.concat(generateRows(clone(cords), 0, -2 * scanPoints, southCount, fuel.South, scanPoints));
 
     const uniqueCords = [];
     const map = new Map();
+
     for (const cord of allCords) {
-        let sign = cord.X+""+cord.Y+""+cord.Z;
+        let sign = cord.Cords.X+""+cord.Cords.Y+""+cord.Cords.Z;
         if(!map.has(sign)){
             map.set(sign, true);    // set any value to Map
-
-            if (getDistance(cord, cords)<= fuel)
-                uniqueCords.push(cord);
+            uniqueCords.push(cord);
         }
     }
-
-
 
     return uniqueCords;
 }
 
-function buildRow(cords, range) {
+function generateRows(cords, x, y, count, topRange, scanPoints) {
+    let allCords = [];
+
+    for (let i = 1; i < count; i++) {
+        allCords = allCords.concat(
+            buildRow(add(clone(cords), x * i, y * i, 0), scanPoints, topRange, cords));
+    }
+    return allCords;
+}
+
+
+function buildRow(cords, range, topRange, baseCords) {
+    console.log(cords);
     var thisLists = [];
 
     // Right, Left, Top, Bottom
-    thisLists.push(addX(clone(cords), range));
-    thisLists.push(addX(clone(cords), -range));
-    thisLists.push(addY(clone(cords), range));
-    thisLists.push(addY(clone(cords), -range));
+    thisLists.push(add(clone(cords), range, 0, 0));
+    thisLists.push(add(clone(cords), -range, 0, 0));
+    thisLists.push(add(clone(cords), 0, range, 0));
+    thisLists.push(add(clone(cords), 0, -range, 0));
 
     // Above - Right, Left, Top, Bottom
-    thisLists.push(addZ(addX(clone(cords), range), range));
-    thisLists.push(addZ(addX(clone(cords), range), -range));
-    thisLists.push(addZ(addY(clone(cords), range), range));
-    thisLists.push(addZ(addY(clone(cords), range), -range));
+    thisLists.push(add(clone(cords), range, 0, range));
+    thisLists.push(add(clone(cords), range, 0, -range));
+    thisLists.push(add(clone(cords), 0, range, range));
+    thisLists.push(add(clone(cords), 0, range, -range));
 
     // Below - Right, Left, Top, Bottom
-    thisLists.push(addZ(addX(clone(cords), -range), range));
-    thisLists.push(addZ(addX(clone(cords), -range), -range));
-    thisLists.push(addZ(addY(clone(cords), -range), range));
-    thisLists.push(addZ(addY(clone(cords), -range), -range));
+    thisLists.push(add(clone(cords), -range, 0, range));
+    thisLists.push(add(clone(cords), -range, 0, -range));
+    thisLists.push(add(clone(cords), 0, -range, range));
+    thisLists.push(add(clone(cords), 0, -range, -range));
+
+    const rangeCords = [];
+    const map = new Map();
+    for (const cord of thisLists) {
+        let sign = cord.X+""+cord.Y+""+cord.Z;
+        if(!map.has(sign)){
+            map.set(sign, true);    // set any value to Map
+            let dis = getDistance(cord, baseCords);
+            if (dis <= topRange)
+                rangeCords.push({Cords: cord, Distance: dis});
+        }
+    }
+
+    return rangeCords;
+}
+
+function mainCords(cords, range) {
+    var thisLists = [];
+    thisLists.push({Cords: add(clone(cords), 0, 0, 0), Distance: 0});
+
+    let topCords = add(clone(cords), 0, 0, range);
+    let dis = getDistance(cords, topCords);
+    thisLists.push({Cords: topCords, Distance: dis});
+
+    let bottomCords = add(clone(cords), 0, 0, -range);
+    dis = getDistance(cords, bottomCords);
+    thisLists.push({Cords: bottomCords, Distance: dis});
 
     return thisLists;
 }
@@ -235,12 +275,12 @@ function Network(name, level, cords, range) {
 }
 
 function createString(networkObject) {
-    return `${networkObject.Name}&${networkObject.Scanner}&${networkObject.Cords.X}&${networkObject.Cords.Y}&${networkObject.Cords.Z}&${networkObject.Range}`;
+    return `${networkObject.Name}&${networkObject.Scanner}&${networkObject.Cords.X}&${networkObject.Cords.Y}&${networkObject.Cords.Z}&${networkObject.Range.West}&${networkObject.Range.North}&${networkObject.Range.East}&${networkObject.Range.South}`;
 }
 
 function readString(networkString) {
     let parts = networkString.split('&');
-    return Network(parts[0], parts[1], Cords(parts[2], parts[3], parts[4]), parts[5]);
+    return Network(parts[0], parts[1], Cords(parts[2], parts[3], parts[4]), Range(parts[5], parts[6], parts[7], parts[8]));
 }
 
 function saveNode(network) {
@@ -290,17 +330,9 @@ function clone(cords) {
     return {X : cords.X, Y: cords.Y, Z: cords.Z}
 }
 
-function addX(cords, x) {
+function add(cords, x, y, z) {
     cords.X = cords.X + x;
-    return cords;
-}
-
-function addY(cords, y) {
     cords.Y = cords.Y + y;
-    return cords;
-}
-
-function addZ(cords, z) {
     cords.Z = cords.Z + z;
     return cords;
 }
@@ -323,6 +355,12 @@ function isEqual(cordsA, cordsB) {
 
 // End Cords Class
 
+// Range Class
+
+function Range(l,t,r,b) {
+    return {West: l, North: t, East: r, South: b};
+}
+
 // Fleet Class
 
 function Fleet(id, cords, status) {
@@ -335,8 +373,10 @@ function Fleet(id, cords, status) {
 
 function htmlIframe(id, name, sensor, cords, range) {
     if (!cords) cords = {X:"",Y:"",Z:""};
+    if (!range) range = Range('','','','');
 
-    return `<div id="manage-data${id}">
+    return `
+<div id="manage-data${id}">
 	<div id="header-data${id}">
 		<table id="table-header${id}" style="width:100%">
 			<tr class="dark tbborder">
@@ -352,59 +392,67 @@ function htmlIframe(id, name, sensor, cords, range) {
 				</td>
 			</tr>
 			<tr class="dark tbborder">
-				<td class="padding5">Core Point:</td>
+				<td class="padding5">Core Point (X Y Z):</td>
 				<td class="padding5">
-					<input type="text" class="darkinput" id="core-pointX${id}" value='${cords.X}' />
-<input type="text" class="darkinput" id="core-pointY${id}" value='${cords.Y}' />
-<input type="text" class="darkinput" id="core-pointZ${id}" value='${cords.Z}' />
+					X:<input type="text" class="darkinput" id="core-pointX${id}" value='${cords.X}' />
+					Y:<input type="text" class="darkinput" id="core-pointY${id}" value='${cords.Y}' />
+					Z:<input type="text" class="darkinput" id="core-pointZ${id}" value='${cords.Z}' />
 				</td>
 			</tr>
 			<tr class="dark tbborder">
 				<td class="padding5">Range:</td>
 				<td class="padding5">
-					<input type="text" class="darkinput" id="range${id}" value='${range}' />
+					West:<input type="text" class="darkinput" id="rangeL${id}" value='${range.West}' />
+North:<input type="text" class="darkinput" id="rangeT${id}" value='${range.North}' />
+East:<input type="text" class="darkinput" id="rangeR${id}" value='${range.East}' />
+South:<input type="text" class="darkinput" id="rangeB${id}" value='${range.South}' />
 				</td>
 			</tr>
 			<tr class="dark tbborder">
 				<td>
 					<input type="button" id='buttonUpdate${id}' value="${id == 0 ? 'Add' : 'Update'}" class="darkbutton dangerbutton" />
 					<input type='button' id='buttonDelete${id}' class='darkbutton dangerbutton' value='Delete' />
-<input type="button" id='buttonView${id}' class="darkbutton dangerbutton" value="View" />
+					<input type="button" id='buttonView${id}' class="darkbutton dangerbutton" value="View" />
 				</td>
 			</tr>
 		</table>
 	</div>
-
 </div>`;
-
 }
 
 function addHeader() {
-    return `<div id="body-data">
-		<table id="table-data" style="width:100%">
-			<thead class="dark tbborder">
-				<td class="padding5" align="center">Id</td>
-<td class="padding5" align="center">Name</td>
-				<td class="padding5" align="center">Cords</td>
-				<td class="padding5" align="center">Distance</td>
-				<td class="padding5" align="center">Sensor Status</td>
-				<td class="padding5" align="center">Fleet</td>
-<td><input id='verify-all' type='button' value='Verify All' class="darkbutton dangerbutton" /></td>
-			</thead>
-	</div>`;
+    return `
+<div id="body-data">
+	<table id="table-data" style="width:100%">
+		<thead class="dark tbborder">
+			<td class="padding5" align="center">Id</td>
+			<td class="padding5" align="center">Name</td>
+			<td class="padding5" align="center">Cords</td>
+			<td class="padding5" align="center">Distance</td>
+			<td class="padding5" align="center">Sensor Status</td>
+			<td class="padding5" align="center">Fleet</td>
+			<td>
+				<input id='verify-all' type='button' value='Verify All' class="darkbutton dangerbutton" />
+			</td>
+		</thead>
+</div>`
 }
 
-function newRaw(index, cords, distance) {
-    return `<tr class="dark tbborder">
-        <td align="center">${index}</td>
-<td align="center"><input type="text" class="darkinput" placeholder="Fleet Name For Bookmarking" value="" /></td>
-    <td align="center">${toCords(cords)}</td>
-    <td align="center">${toNumber(distance)} km</td>
-    <td align="center">Check</td>
-    <td align="center"><input type="text" class="darkinput" placeholder="Add Fleet Number" value="" /></td>
-<td><input type="text" class="darkbutton" value="Send" id="sendButton${index}" />
-<input type="text" class="darkbutton" value="Bookmark" id="bookmark${index}" /></td>
-    </tr>`;
+function newRaw(index, cords) {
+    
+return `
+<tr class="dark tbborder">
+	<td align="center">${index}</td>
+	<td align="center"><input type="text" class="darkinput" placeholder="Bookmark Name" value="" /></td>
+	<td align="center">${toCords(cords.Cords)}</td>
+	<td align="center">${toNumber(cords.Distance)} km</td>
+	<td align="center">Check</td>
+	<td align="center"><input type="text" class="darkinput" placeholder="Add Fleet Number" value="" /></td>
+	<td>
+		<input type="text" class="darkbutton" value="Send" id="sendButton${index}" />
+		<input type="text" class="darkbutton" value="Bookmark" id="bookmark${index}" />
+	</td>
+</tr>`
 }
 
 // End HTML Painting Functions
@@ -446,4 +494,3 @@ function toNumber(num) {
 }
 
 // End Utilities Functions
-
